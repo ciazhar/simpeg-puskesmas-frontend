ab7d9325bc88aeffc63b9c9332e92a74d9793b11
ciazhar <ciazhar.id@gmail.com>
2018-06-04 23:50:27 +0700

    fix register

--- js/auth.js
+++ js/auth.js
@@ -1,11 +1,12 @@
 var app = angular.module('mainModule', []);
-var LOGIN_URL = 'http://dinkes.batangkab.go.id:8080/login'
-var REGISTER_URL = 'http://dinkes.batangkab.go.id:8080/account/submit'
+var API_LOGIN_URL = 'http://dinkes.batangkab.go.id:8080/login'
+var API_REGISTER_URL = 'http://dinkes.batangkab.go.id:8080/account/users/submit'
 var HOME_URL = 'pegawai.html'
+var LOGIN_URL = 'login.html'
 
 app.controller('userController', function($scope, $http, $window){
   $scope.doLogin = function () {
-    $http.post(LOGIN_URL, $scope.login).then(sukses,gagal);
+    $http.post(API_LOGIN_URL, $scope.login).then(sukses,gagal);
     function sukses(response) {
         $window.localStorage.setItem('authorization',response.data.token);
         $window.location.href = HOME_URL;
@@ -15,9 +16,14 @@ app.controller('userController', function($scope, $http, $window){
     };
   };
   $scope.doRegister = function () {
-    $http.post(REGISTER_URL, $scope.register).then(sukses,gagal);
+    if($scope.register.password != $scope.register.confirmPassword){
+        console.log('Password Tidak Sama');
+        return;
+    }
+    $http.post(API_REGISTER_URL, $scope.register).then(sukses,gagal);
     function sukses(response) {
-        console.log('Resoonse : '+response);
+        $window.localStorage.setItem('id',response.data.id);
+        $window.location.href = LOGIN_URL;
     };
     function gagal(response) {
         console.log('Error : '+response);
