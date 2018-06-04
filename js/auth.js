071691bb21bc3a1901b28a4398b28873ef355f08
ciazhar <ciazhar.id@gmail.com>
2018-06-04 23:34:46 +0700

    fix login

--- js/auth.js
+++ js/auth.js
@@ -1,10 +1,26 @@
 var app = angular.module('mainModule', []);
+var LOGIN_URL = 'http://dinkes.batangkab.go.id:8080/login'
+var REGISTER_URL = 'http://dinkes.batangkab.go.id:8080/account/submit'
+var HOME_URL = 'pegawai.html'
 
-app.controller('mainController', function($scope, $http){ //o scope liga o js e o template
-  $scope.nome = 'Valor Inicial';
-  //$http.get().success();
-  $scope.reset = function()
-  {
-    $scope.nome = '';
-  }
+app.controller('userController', function($scope, $http, $window){
+  $scope.doLogin = function () {
+    $http.post(LOGIN_URL, $scope.login).then(sukses,gagal);
+    function sukses(response) {
+        $window.localStorage.setItem('authorization',response.data.token);
+        $window.location.href = HOME_URL;
+    };
+    function gagal(response) {
+        console.log('Error : '+response);
+    };
+  };
+  $scope.doRegister = function () {
+    $http.post(REGISTER_URL, $scope.register).then(sukses,gagal);
+    function sukses(response) {
+        console.log('Resoonse : '+response);
+    };
+    function gagal(response) {
+        console.log('Error : '+response);
+    };
+  };
 });
