23e68cb8706287bf02cf8cd07eb6c1a9b974b0ad
ciazhar <ciazhar.id@gmail.com>
2018-06-26 07:17:31 +0700

    save pegawai

--- js/pegawai.js
+++ js/pegawai.js
@@ -15,6 +15,13 @@ app.controller('pegawaiController',function ($scope, $http, $window) {
         }
     };
     $scope.listPegawai();
+
+    var urlRedirect = "form-pegawai.html";
+    $scope.editPegawai = function (pegawai) {
+        console.log(pegawai.id)
+        $window.localStorage.setItem('id',pegawai.id);
+        $window.location.href = urlRedirect;
+    }
     
     $scope.hapusPegawai = function(pegawai){
         $http.post(BASE_URL+'/pegawai/'+pegawai.id).then(sukses,gagal);
@@ -27,4 +34,40 @@ app.controller('pegawaiController',function ($scope, $http, $window) {
         $window.location.reload();
     };
 
+})
+
+app.controller('formController',function ($scope, $http, $window) {
+    var urlListPegawai = "pegawai.html";
+    var idnya = $window.localStorage.getItem('id');
+
+    $scope.formPegawai = function () {
+        $http.get(BASE_URL+'/pegawai/'+idnya).then(sukses, gagal);
+        function sukses(response){
+            $scope.findedPegawai = response.data.data;
+        }
+        function gagal(response){
+            console.log('Error'+response);
+        };
+        delete sessionStorage.id;/// delete id agar tidak terjadi redundan
+    };
+    $scope.formPegawai();
+
+    $scope.simpanPegawai = function () {
+        ///Kondisi ini untuk mengantisipasi tidak terdeteksinya value di field sehingga tidak menjadi json utuh
+        $scope.pegawai.id = $scope.findedPegawai.id;
+        if ($scope.pegawai.noKtp == null){
+            $scope.pegawai.noKtp = $scope.findedPegawai.noKtp;
+        }
+        if ($scope.pegawai.gaji == null){
+            $scope.pegawai.gaji = $scope.findedPegawai.gaji;
+        }
+
+        $http.post(BASE_URL+'/pegawai', $scope.pegawai).then(sukses,gagal);
+        function sukses(response) {
+            $window.location.href = urlListPegawai;
+        };
+        function gagal(response) {
+            console.log('Error'+response);
+        };
+    };
 })
